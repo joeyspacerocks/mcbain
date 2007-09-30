90676b6075c506bea3fc4f9478efebe525346846
joe.trewin <joe.trewin@fa9a6fad-f637-0410-b9f2-59221c3c02b4>
2007-09-30 10:15:16 +0000

    Added map of blogs and empty example.

--- trunk/mcbain/examples/blog/src/org/mcbain/examples/blog/model/BlogService.java
+++ trunk/mcbain/examples/blog/src/org/mcbain/examples/blog/model/BlogService.java
@@ -16,6 +16,8 @@ package org.mcbain.examples.blog.model;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /************************************************************************
  * Service providing access to blogs.
@@ -26,7 +28,7 @@ import java.util.GregorianCalendar;
 
 public class BlogService {
 
-    private Blog blog;
+    private Map<String,Blog> blogs;
     
     
     /************************************************************************
@@ -36,10 +38,17 @@ public class BlogService {
     public BlogService() {
         Calendar cal = new GregorianCalendar(2006, 6, 2);
         
-        blog = new Blog("Joe");
-        blog.addPost("Folly", "If there are two things that I've learnt over the years it's that there are too many Java web frameworks, and that you should never mess about with classloaders. That said, this blog is an on-going account of my attempt to design and build a better web framework. The first challenge is bringing a bit more dynasism to Java using custom classloaders. If you haven't already guessed, this is (hopefully) going to get a bit technical. If Java, web programming and all the associated nonsense doesn't float your boat you'd possible be better off clicking the Next Blog button now. Similarly, if you chanced upon this in the hope of finding evocations literally both strange and curved, you will again be disappointed.", cal.getTime());
-        blog.addPost("What's already out there?", "What's the first thing you do when evaluating a web framework? For me, it's to ignore the paragraphs on the homepage that always seem to contain the words ease, leverage and seperation of concerns, and find the HelloWorld or equivilent tutorial. It then takes me several seconds to decide whether it's for me:");
-        blog.addPost("Stripes before my eyes", "<b>Responding to my previous</b> &lt; knee-jerk dismissal of the Stripes framework, Tim Fennell commented: \"You shouldn't dismiss Stripes so quickly. All the examples use JSP because, frankly, that's what the vast majority of developers are familiar with. But Stripes is not tied to JSP.\" So I took some time to take a closer look at Stripes, and I have to say that I like what I see.");
+        blogs = new HashMap<String,Blog>();
+        
+        Blog populated = new Blog("Joe");
+        populated.addPost("Folly", "If there are two things that I've learnt over the years it's that there are too many Java web frameworks, and that you should never mess about with classloaders. That said, this blog is an on-going account of my attempt to design and build a better web framework. The first challenge is bringing a bit more dynasism to Java using custom classloaders. If you haven't already guessed, this is (hopefully) going to get a bit technical. If Java, web programming and all the associated nonsense doesn't float your boat you'd possible be better off clicking the Next Blog button now. Similarly, if you chanced upon this in the hope of finding evocations literally both strange and curved, you will again be disappointed.", cal.getTime());
+        populated.addPost("What's already out there?", "What's the first thing you do when evaluating a web framework? For me, it's to ignore the paragraphs on the homepage that always seem to contain the words ease, leverage and seperation of concerns, and find the HelloWorld or equivilent tutorial. It then takes me several seconds to decide whether it's for me:");
+        populated.addPost("Stripes before my eyes", "<b>Responding to my previous</b> &lt; knee-jerk dismissal of the Stripes framework, Tim Fennell commented: \"You shouldn't dismiss Stripes so quickly. All the examples use JSP because, frankly, that's what the vast majority of developers are familiar with. But Stripes is not tied to JSP.\" So I took some time to take a closer look at Stripes, and I have to say that I like what I see.");
+        
+        blogs.put("joe", populated);
+        
+        Blog empty = new Blog("Empty");
+        blogs.put("empty", empty);
     }
     
     
@@ -51,6 +60,6 @@ public class BlogService {
      */
     
     public Blog getBlog(String name) {
-        return (blog.getName().equalsIgnoreCase(name) ? blog : null);
+        return blogs.get(name.toLowerCase());
     }
 }
